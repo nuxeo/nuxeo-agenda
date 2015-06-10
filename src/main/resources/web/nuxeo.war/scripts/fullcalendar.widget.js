5d57565514cfafa9653de6cf513ff95a967fc4e2
Vincent Dutat <vdutat@nuxeo.com>
2015-06-09 18:49:51 -0700

    NXP-17229 nuxeo-agenda addon: 'calendar' widget does not retrieve all the events for the current month/week/day

--- src/main/resources/web/nuxeo.war/scripts/fullcalendar.widget.js
+++ src/main/resources/web/nuxeo.war/scripts/fullcalendar.widget.js
@@ -12,7 +12,7 @@ function nuxeoAgendaLoadEvents(from, to, callback) {
 	// build default operation for Document
 	temp.providerName = "agenda_list_events_with_date_query";
 	
-	temp.queryParams=currentDocumentId+","+from.toISOString()+","+to.toISOString();
+	temp.queryParams = from.toISOString() + "," + to.toISOString();
 	
 	// temp.providerName = params.pageProviderName;
 	temp.page = "0";
@@ -25,6 +25,9 @@ function nuxeoAgendaLoadEvents(from, to, callback) {
 		temp.conversationId = currentConversationId;
 	}
 	op.addParameters(temp);
+    op.setHeaders({
+      "X-NXenrichers.document": "documentURL"
+    });
 	
 	op.execute(function(data, textStatus, xhr) {
 		var eventsArray = Array();
