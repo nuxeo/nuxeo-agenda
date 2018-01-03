34321e04270e917934acd6f3b04425843467e26b
Kevin Leturc <kleturc@nuxeo.com>
2018-01-03 15:01:22 +0100

    NXP-24090: Replace commons-lang by commons-lang3

--- nuxeo-agenda-core/src/main/java/org/nuxeo/ecm/agenda/AgendaComponent.java
+++ nuxeo-agenda-core/src/main/java/org/nuxeo/ecm/agenda/AgendaComponent.java
@@ -22,7 +22,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
